ea88bb45696ba7638dee3931dd3321bb035bd555
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-01-21 17:57:21 +0200

    initializing project folder

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init(elems, {});
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
